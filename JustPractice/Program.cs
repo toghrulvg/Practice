8670ec355436e355dc5adece201c6c5cecee87c2
toghrulvg <toghrulvg@code.edu.az>
2022-06-23 00:03:31 +0400

    commit

---
+++ JustPractice/Program.cs
@@ -0,0 +1,136 @@
+﻿//using System;
+
+//    namespace JustPractice
+//    {
+//    class JustPractice
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            //    //string[] words = new string[4];
+
+//            //    //words[0] = "sifir";
+//            //    //words[1] = "bir";
+//            //    //words[2] = "iki";
+
+//            //    //Array.Resize(ref words, 9);
+//            //    //words[8] = "sekkiz";
+
+
+//            //    //foreach (var item in words)
+//            //    //{
+//            //    //    Console.WriteLine(item);
+//            //    //}
+//            //    //int[,] arr =
+//            //    //{
+//            //    //        {1,2 },
+//            //    //        {3,4 }
+//            //    //};
+//            //    //Console.WriteLine(arr.Rank);
+//            //    //Console.WriteLine(arr[0, 0]);
+
+
+//            //    //string name = "Togrul";
+//            //    //string surname = "Quluzade";
+//            //    //int age = 23;
+
+//            //    //var obj1 = new
+//            //    //{
+//            //    //    name = "Togrul",
+//            //    //    surname = "Quluzade",
+//            //    //    age = 23
+//            //    //};
+//            //    //var obj2 = new
+//            //    //{
+//            //    //    name = "Lionel",
+//            //    //    surname = "Messi",
+//            //    //    age = 34
+//            //    //};
+//            //    //Console.WriteLine($"{obj2.name} {obj2.surname} {obj2.age}");
+
+//            //    Student st1 = new Student("Sabir,25");
+//            //    //st1.name = "Togrul";
+//            //    //st1.surname = "Quluzade";
+//            //    //st1.age = 23;
+
+//            //    //Student st2 = new Student();
+//            //    //st2.name = "Lionel";
+//            //    //st2.surname = "Messi";
+//            //    //st2.age = 34;
+
+//            //    //Console.WriteLine(st2.surname);
+//            //    //Console.WriteLine(st2.Fullname());
+//            //    //Console.WriteLine(st2.FullData());
+
+//            //    //Student st3 = new Student
+//            //    //{
+//            //    //    name = "Azer",
+//            //    //    surname = "Ferhadov",
+//            //    //    age = 38
+//            //    //};
+//            //}
+//            //class Student
+//            //{
+//            //    public string name;
+//            //    public string surname;
+//            //    public int age;
+//            //    public string address;
+
+//            //    public Student()
+//            //    {
+
+//            //        Console.WriteLine("Empty");
+//            //    }
+
+//            //    public Student(int age, string address ) :this(address)
+//            //    {
+//            //        Console.WriteLine("Age" + age);
+//            //    }
+
+//            //    public Student(string adds):this(true)
+//            //    {
+//            //        Console.WriteLine("Name" + stuName);
+//            //    }
+
+//            //    public Student(bool isMarried)
+//            //    {
+//            //        Console.WriteLine("Married");
+//            //    }
+
+
+
+
+
+
+
+//            //public string Fullname()
+//            //{
+//            //    return name + " " + surname;
+//            //}
+//            //public string FullData()
+//            //{
+//            //    return name + " " + surname + " " + age;
+//            //}
+
+//            public string GetGroupInfo()
+//            {
+//                Teacher teacher = new Teacher();
+//                teacher.name = "Cavid";
+//                return $"Teacher name : {teacher.GetTeacherName}" Student name: {name};
+//            }
+
+
+//        }
+//        class Teacher
+//        {
+//            public string name;
+//            public float salary;
+
+//            public string GetTeacherName()
+//            {
+//                return name;
+//            }
+//        }
+      
+//    }    
+//}   
